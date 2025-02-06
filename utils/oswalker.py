8cc540d5fdf2dd829e3757786c64c83a1a1d2da4
Pratham Rangunwala <prathamdj777@gmail.com>
2025-02-06 18:40:13 +0530

    Co-authored-by: Mohking1 <Mohking1@users.noreply.github.com>

---
+++ utils/oswalker.py
@@ -0,0 +1,34 @@
+import os
+
+BLOCKED_DIRS = {'.git', '__pycache__', 'node_modules', '.venv'}
+def is_valid_dir(path):
+    path_parts = path.split(os.sep)
+    return not any(part in BLOCKED_DIRS for part in path_parts)
+
+
+CODE_EXTENSIONS = [".c", ".cpp", ".h", ".hpp", ".java", ".py",".rb", ".rs", ".go", ".js", ".ts", ".cs", ".swift", ".kt", ".m", ".php"]
+def valid_extension(path):
+    if any(path.endswith(ext) for ext in CODE_EXTENSIONS) :
+        return path
+
+def filter_path(path):
+    dir_path = os.path.dirname(path)
+    return(
+        is_valid_dir(dir_path) and
+        valid_extension(path)
+    )
+
+def find_files(root_path) :
+    file_paths = []
+    for (dirpath, dirnames, filenames) in os.walk(root_path) :
+        for file in filenames:
+            file_path = os.path.join(dirpath, file)
+            if filter_path(file_path):
+                file_paths.append(file_path)
+    return file_paths
+
+root_path=input("enter path")
+print(find_files(root_path))   
+
+
+
